ea4854f55272eaacfca71856a1d4cc471937f328
zekromrevolution <101708103+zekromrevolution@users.noreply.github.com>
2023-06-09 21:47:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,42 @@
-from flask import Flask, request, jsonify, render_template
-import requests
-import io
-import importlib
+import io, rembg, requests
 
-app = Flask(__name__)
+from PIL import Image
 
-def install_package(package):
-    try:
-        importlib.import_module(package)
-    except ImportError:
-        import subprocess
-        subprocess.call(['pip', 'install', package])
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
 
+app = FastAPI()
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-       
-        
-        
-@app.route('/remove-background', methods=['GET'])
-def remove_background():
-    try:
-        import rembg
-    except ImportError:
-        install_package('rembg')
-        import rembg
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+
+async def home(request: Request):
+
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/remove-background-url")
+
+async def remove_background_url(image_url: str):
+
+    image = requests.get(image_url).content
 
     try:
-        import PIL
-    except ImportError:
-        install_package('Pillow')
-        import PIL
 
-    from PIL import Image
-
-    
-    image_url = request.args.get('image')
-    image = requests.get(f"https://{image_url}").content
-
-    try:
         output = rembg.remove(image)
+
         output_image = Image.open(io.BytesIO(output))
+
         cropped_image = output_image.crop(output_image.getbbox())  # Crop to the maximum extent
+
         cropped_image_bytes = io.BytesIO()
+
         cropped_image.save(cropped_image_bytes, format='PNG')
+
         cropped_image_bytes.seek(0)
+
     except Exception as e:
-        return jsonify({'error': str(e)})
 
-    return jsonify({'result': cropped_image_bytes.getvalue()})
+        return {'error': str(e)}
 
-if __name__ == '__main__':
-    try:
-        import flask
-    except ImportError:
-        install_package('flask')
-        import flask
-
-    app.run(host='0.0.0.0', port=8080)
+    return {'result': cropped_image_bytes.getvalue()}
