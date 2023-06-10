d65d26d8177402773009223ed0f12686b611841f
zekromrevolution <101708103+zekromrevolution@users.noreply.github.com>
2023-06-10 08:41:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import io, rembg, requests
 
 from PIL import Image
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
