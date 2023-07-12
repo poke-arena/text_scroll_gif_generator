8043e0be94352aa7396636ba88ad9a2bdd1ab29c
zekromrevolution <101708103+zekromrevolution@users.noreply.github.com>
2023-07-13 01:19:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,16 +30,16 @@ async def img(url: str):
 @app.get("/text_to_gif")
 async def text_to_gif(text: str, theme: str="dark", custom_text_color="#000000", body_color="#FFFFFF", custom_icon=None):
     if theme.lower() == "light":
-        announce_bg = await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128685099584589834/Untitled186_20230712192107.png")
-        announce_mask = await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128685099311955988/Untitled186_20230712192116.png")
+        announce_bg = (await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128685099584589834/Untitled186_20230712192107.png")).copy()
+        announce_mask = (await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128685099311955988/Untitled186_20230712192116.png")).copy()
         text_color = "#37383A"
     elif theme.lower() == "dark":
-        announce_mask = await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128681074743058452/Untitled186_20230712190358.png")
-        announce_bg = await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128681074290081882/Untitled186_20230712190350.png")
+        announce_mask = (await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128681074743058452/Untitled186_20230712190358.png")).copy()
+        announce_bg = (await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128681074290081882/Untitled186_20230712190350.png")).copy()
         text_color = "#FFFFFF"
     elif theme.lower() == "custom":
-        announce_mask = await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128681074743058452/Untitled186_20230712190358.png")
-        announce_bg = await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128681074290081882/Untitled186_20230712190350.png")
+        announce_mask = (await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128681074743058452/Untitled186_20230712190358.png")).copy()
+        announce_bg = (await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128681074290081882/Untitled186_20230712190350.png")).copy()
         new_mask = Image.new("RGBA", (announce_bg.size[0], announce_bg.size[1]), body_color)
         text_color = custom_text_color
         announce_mask.paste(new_mask, mask=new_mask)
