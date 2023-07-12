3c8c98fadc704806092da383fe8798f6ea0d0096
zekromrevolution <101708103+zekromrevolution@users.noreply.github.com>
2023-07-13 01:30:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ async def text_to_gif(text: str, theme: str="dark", custom_text_color="#000000",
         announce_bg = (await img("https://cdn.discordapp.com/attachments/1043603765212749944/1128681074290081882/Untitled186_20230712190350.png")).copy()
         new_mask = Image.new("RGBA", (announce_bg.size[0], announce_bg.size[1]), body_color)
         text_color = custom_text_color
-        announce_mask.paste(new_mask, mask=new_mask)
-        announce_bg.paste(new_mask, mask=new_mask)
+        announce_mask.paste(new_mask, mask=announce_mask)
+        announce_bg.paste(new_mask, mask=announce_bg)
         if custom_icon is not None:
             icon = await img(custom_icon)
             height = announce_mask.size[1] - 4
